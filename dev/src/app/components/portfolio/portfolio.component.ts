50c5f0fb4e73ab35adaa7963e353bf2124328b78
doms123 <dominicksanchez30@gmail.com>
2018-07-25 22:07:11 +0800

    updates

--- dev/src/app/components/portfolio/portfolio.component.ts
+++ dev/src/app/components/portfolio/portfolio.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  baseUrl: '/porfolio'
-  
+  baseUrl:string = '/porfolio';
+
   constructor() { }
 
   ngOnInit() {
--- dev/src/app/components/work/work.component.ts
+++ dev/src/app/components/work/work.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
-  baseUrl: '/porfolio'
-  
+  baseUrl:string = '/porfolio';
+
   constructor() { }
 
   ngOnInit() {
